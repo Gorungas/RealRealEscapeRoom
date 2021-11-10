88c85506fba03501f5005a7f93b90997fd664360
Dianhanluosi <evanzheng1127@gmail.com>
2021-11-10 13:49:05 -0500

    room stuff

---
+++ Assets/Code/DoorHinge.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorHinge : MonoBehaviour
+{
+
+    Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("hand"))
+        {
+            rb.isKinematic = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("hand"))
+        {
+            rb.isKinematic = true;
+        }
+    }
+}
